a07d76fd52ddc0ecca9e8c3921e6042a4da2076a
Pavel Pogodin <pavel.pogodin@gmail.com>
2026-02-17 22:25:55 +0300

    Update onstart_slave.sh

--- ext_tree/board/luckfox/rootfs_overlay/etc/networkaudiod/onstart_slave.sh
+++ ext_tree/board/luckfox/rootfs_overlay/etc/networkaudiod/onstart_slave.sh
@@ -8,15 +8,15 @@ if [ ! -f "$TX_RESET" ]; then
     exit 0
 fi
 
-# 1. Force precise time sync with master BEFORE playback starts
+#    Reset I2S TX with precise timing
+echo "1" > "$TX_RESET"
+
+#    Force precise time sync with master BEFORE playback starts
 #    burst 4/4: get 4 fresh NTP measurements from master
 #    waitsync: wait up to 10 tries until offset < 1ms
 #    makestep: snap clock to master's exact time
 chronyc burst 4/4
 chronyc waitsync 10 0.001
 chronyc makestep
-
-# 2. Reset I2S TX with precise timing
-echo "1" > "$TX_RESET"
 
 exit 0
